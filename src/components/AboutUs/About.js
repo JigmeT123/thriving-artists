3c4df1826f72f7d1d6496c19a6c1ca8ba45d2019
jigmet12 <jigmetashi02@gmail.com>
2020-12-18 13:57:01 +0400

    FEAT: added the description

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -12,10 +12,10 @@ const About = () => {
                     <Button className={styles.supportBtn}>Artist Profile</Button>
                 </div>
                 <div className={styles.aboutCardContainer1}>
-                    <AboutCard className={styles.card1} description=""/>
-                    <AboutCard className={styles.card2} description=""/>
-                    <AboutCard className={styles.card3} description=""/>
-                    <AboutCard className={styles.card4} description=""/>
+                    <AboutCard className={styles.card1} description="Thriving Artists is a project designed to gather young, thriving artists and art enthusiasts from all over Azerbaijan in one platform. We place the artworks of the young artists to give them an opportunity to showcase their work, to help them gain recognition and earn money by being engaged in a field they love, art."/>
+                    <AboutCard className={styles.card2} description="Considering the fact that the field of art is still under development in Azerbaijan, where artists do not get enough recognition for their creativity and are obliged to promote themselves, we decided to create this platform to gather the works of these artists in a single online gallery, so that they can showcase their works without being concerned about self-promotion."/>
+                    <AboutCard className={styles.card3} description="This platform is designed as a combination of an art gallery and a shopping website. Art enthusiasts can view the art pieces, read about them in the gallery section, and get to know the artists better by visiting their profiles. Viewers who want to purchase works will have the contact information of the artist and will buy the item directly from the creator."/>
+                    <AboutCard className={styles.card4} description="Thriving Artists is not only an exhibition platform for the art creators but also an online store for the art collectors, buyers, and gift-givers who enjoy receiving and/or giving a piece of art. The prices of the works are determined by the artists themselves, not by the creators of the platform. So at the end of the day, the buyer will not only receive well-priced, handmade artwork but also end up supporting the careers of local artists."/>
                 </div>
                 
             </div>
--- src/components/aboutCard/AboutCard.js
+++ src/components/aboutCard/AboutCard.js
@@ -1,10 +1,10 @@
 import styles from './about.module.css';
-import {Card} from '@material-ui/core';
+import {Card, Typography} from '@material-ui/core';
 
 const AboutCard = ({description}) => {
     return (
         <Card className={styles.aboutCardContainer}>
-           {description}
+           <Typography>{description}</Typography>
         </Card>
     )
 }
