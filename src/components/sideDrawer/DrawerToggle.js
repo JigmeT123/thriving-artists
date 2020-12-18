b388258fc443cafda8cbe930100b6640f824bdc0
jigmet12 <jigmetashi02@gmail.com>
2020-12-18 16:34:16 +0400

    FEAT: header fixed the size and the menu icons size

--- src/components/sideDrawer/DrawerToggle.js
+++ src/components/sideDrawer/DrawerToggle.js
@@ -4,7 +4,7 @@ import {Menu} from '@material-ui/icons'
 const DrawerToggle = ({click}) => {
     return (
        <button className={styles.toggleBtn} onClick={click}>
-           <Menu />
+           <Menu className={styles.menuBtn}/>
        </button>
     )
 }
